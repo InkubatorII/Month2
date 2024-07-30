cb3fb73219535bd9c15dcc0499d847e1b4b43372
InkubatorII <holloward@gmail.com>
2024-07-30 20:37:05 +0600

    Lesson_1

---
+++ lesson/lesson_1-1.py
@@ -0,0 +1,14 @@
+import Lesson_1
+import colorama
+import art
+print(colorama.Back.BLACK,colorama.Fore.CYAN)
+print('hello world')
+print(Lesson_1.p)
+
+from art import tprint
+
+
+tprint('Hello world')
+
+
+
